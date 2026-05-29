ca07edcc5e942ca0aa873d265541fd6364774b1c
Brahim <brahimaz@hotmail.com>
2026-05-29 17:15:21 +0200

    User interface added

--- app.py
+++ app.py
@@ -3,181 +3,183 @@ import pandas as pd
 import joblib
 
 # Load trained pipeline
-model = joblib.load("attrition_pipeline.pkl")
-  
+model = joblib.load("attrition_pipeline.joblib")
+st.set_page_config(
+    page_title="Employee Resignation Prediction",
+    layout="centered"
+)
+col1 , col2 =st.columns([2,5])
+with col1:
+    st.image("resign.jpg", width=150)
+with col2:
+    st.markdown(
+        """
+        <h1 style='text-align:center; color:#1f77b4;padding-top:0px;'>
+            Employee Resignation Prediction
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
 
-st.title("Employee Resignation Prediction")
+st.markdown("---")
 
-st.write("Fill employee information to predict attrition risk.")
+st.write("Please Fill employee information to predict attrition risk.")
 
 # =========================
 # CATEGORICAL FEATURES
 # =========================
 
-BusinessTravel = st.selectbox(
+col1 , col2 = st.columns(2)
+
+with col1:
+    BusinessTravel = st.selectbox(
     "Business Travel",
     ['Travel_Rarely', 'Travel_Frequently', 'Non-Travel']
-)
+    )
 
-Department = st.selectbox(
+    Department = st.selectbox(
     "Department",
     ['Research & Development', 'Sales', 'Human Resources']
-)
+    )
 
-EducationField = st.selectbox(
+    EducationField = st.selectbox(
     "Education Field",
     ['Life Sciences', 'Medical', 'Marketing',
      'Technical Degree', 'Other', 'Human Resources']
-)
+    )
 
-Gender = st.selectbox(
+    Gender = st.selectbox(
     "Gender",
     ['Male', 'Female']
-)
-
-JobRole = st.selectbox(
+    )
+with col2:
+    JobRole = st.selectbox(
     "Job Role",
     ['Sales Executive', 'Research Scientist',
      'Laboratory Technician', 'Manufacturing Director',
      'Healthcare Representative', 'Manager',
      'Sales Representative', 'Research Director',
      'Human Resources']
-)
+    )
 
-MaritalStatus = st.selectbox(
+    MaritalStatus = st.selectbox(
     "Marital Status",
     ['Married', 'Single', 'Divorced']
-)
+    )
 
-Over18 = st.selectbox(
+    Over18 = st.selectbox(
     "Over 18",
     ['Y']
-)
+    )
 
-OverTime = st.selectbox(
+    OverTime = st.selectbox(
     "Over Time",
     ['No', 'Yes']
 )
-
 # =========================
 # NUMERICAL FEATURES
 # =========================
+col1 , col2 =st.columns(2)
 
-Age = st.number_input("Age", min_value=18, max_value=65, value=30)
+with col1:
+    Age = st.number_input("Age", min_value=18, max_value=65, value=30)
+    DailyRate = st.number_input("Daily Rate", value=800)
+    DistanceFromHome = st.number_input("Distance From Home", value=10)
+    Education = st.number_input("Education Level", min_value=1, max_value=5, value=3)
+    EmployeeCount = st.number_input("Employee Count", value=1)
+    EmployeeNumber = st.number_input("Employee Number", value=1001)
+    EnvironmentSatisfaction = st.number_input(
+        "Environment Satisfaction",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
+    HourlyRate = st.number_input("Hourly Rate", value=60)
+    JobInvolvement = st.number_input(
+        "Job Involvement",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
+    JobLevel = st.number_input(
+        "Job Level",
+        min_value=1,
+        max_value=5,
+        value=2
+    )
+    JobSatisfaction = st.number_input(
+        "Job Satisfaction",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
+    MonthlyIncome = st.number_input("Monthly Income", value=5000)
+    MonthlyRate = st.number_input("Monthly Rate", value=15000)
+with col2:
+    NumCompaniesWorked = st.number_input(
+        "Number of Companies Worked",
+        value=2
+    )
+    PercentSalaryHike = st.number_input(
+        "Percent Salary Hike",
+        value=15
+    )
+    PerformanceRating = st.number_input(
+        "Performance Rating",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
 
-DailyRate = st.number_input("Daily Rate", value=800)
+    RelationshipSatisfaction = st.number_input(
+        "Relationship Satisfaction",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
+    StandardHours = st.number_input("Standard Hours", value=80)
+    StockOptionLevel = st.number_input(
+        "Stock Option Level",
+        min_value=0,
+        max_value=3,
+        value=1
+    )
+    TotalWorkingYears = st.number_input(
+        "Total Working Years",
+        value=10
+    )
 
-DistanceFromHome = st.number_input("Distance From Home", value=10)
+    TrainingTimesLastYear = st.number_input(
+        "Training Times Last Year",
+        value=2
+    )
 
-Education = st.number_input("Education Level", min_value=1, max_value=5, value=3)
+    WorkLifeBalance = st.number_input(
+        "Work Life Balance",
+        min_value=1,
+        max_value=4,
+        value=3
+    )
 
-EmployeeCount = st.number_input("Employee Count", value=1)
+    YearsAtCompany = st.number_input(
+        "Years At Company",
+        value=5
+    )
 
-EmployeeNumber = st.number_input("Employee Number", value=1001)
+    YearsInCurrentRole = st.number_input(
+        "Years In Current Role",
+        value=3
+    )
 
-EnvironmentSatisfaction = st.number_input(
-    "Environment Satisfaction",
-    min_value=1,
-    max_value=4,
-    value=3
-)
+    YearsSinceLastPromotion = st.number_input(
+        "Years Since Last Promotion",
+        value=1
+    )
 
-HourlyRate = st.number_input("Hourly Rate", value=60)
-
-JobInvolvement = st.number_input(
-    "Job Involvement",
-    min_value=1,
-    max_value=4,
-    value=3
-)
-
-JobLevel = st.number_input(
-    "Job Level",
-    min_value=1,
-    max_value=5,
-    value=2
-)
-
-JobSatisfaction = st.number_input(
-    "Job Satisfaction",
-    min_value=1,
-    max_value=4,
-    value=3
-)
-
-MonthlyIncome = st.number_input("Monthly Income", value=5000)
-
-MonthlyRate = st.number_input("Monthly Rate", value=15000)
-
-NumCompaniesWorked = st.number_input(
-    "Number of Companies Worked",
-    value=2
-)
-
-PercentSalaryHike = st.number_input(
-    "Percent Salary Hike",
-    value=15
-)
-
-PerformanceRating = st.number_input(
-    "Performance Rating",
-    min_value=1,
-    max_value=4,
-    value=3
-)
-
-RelationshipSatisfaction = st.number_input(
-    "Relationship Satisfaction",
-    min_value=1,
-    max_value=4,
-    value=3
-)
-
-StandardHours = st.number_input("Standard Hours", value=80)
-
-StockOptionLevel = st.number_input(
-    "Stock Option Level",
-    min_value=0,
-    max_value=3,
-    value=1
-)
-
-TotalWorkingYears = st.number_input(
-    "Total Working Years",
-    value=10
-)
-
-TrainingTimesLastYear = st.number_input(
-    "Training Times Last Year",
-    value=2
-)
-
-WorkLifeBalance = st.number_input(
-    "Work Life Balance",
-    min_value=1,
-    max_value=4,
-    value=3
-)
-
-YearsAtCompany = st.number_input(
-    "Years At Company",
-    value=5
-)
-
-YearsInCurrentRole = st.number_input(
-    "Years In Current Role",
-    value=3
-)
-
-YearsSinceLastPromotion = st.number_input(
-    "Years Since Last Promotion",
-    value=1
-)
-
-YearsWithCurrManager = st.number_input(
-    "Years With Current Manager",
-    value=4
-)
+    YearsWithCurrManager = st.number_input(
+        "Years With Current Manager",
+        value=4
+    )
 
 # =========================
 # BUILD INPUT DATAFRAME
@@ -233,8 +235,13 @@ if st.button("Predict"):
     if prediction == 1:
         st.error(
             f"Employee likely to resign ({probability:.2f}%)"
+           
         )
     else:
         st.success(
             f"Employee likely to stay ({100 - probability:.2f}%)"
+           
         )
+
+        
+        
